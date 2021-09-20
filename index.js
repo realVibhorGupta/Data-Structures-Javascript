e2dbe46d4574a208c73b181e38abe72b79dc41f8
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-09-20 06:55:34 +0000

    reason added

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ strings.pop();//O(1)
 strings.unshift('e');//O(n)
 //This process takes longer than push and pop
 //The string is indexed in an array. and when we have to add the data in the front , we have to shift all the other elements to n+1 index .This is the reason we are getting the O(n).
+//splice()
+//add data in the middle 
+strings.splice(2,3,'alien');//O(n)
+//reason is the same 
 
 
 console.log(strings);
