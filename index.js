e44fa29403e6f08d621e56d70a96bbcaa8b02fe5
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-09-20 06:49:58 +0000

    use unshift

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
 const strings = ['a','b','c','d'];
+console.log(strings);
+
 strings.push('e');
 strings.pop();
+
+//unshift
+//Add data at the beginning
+//specially in javascript
+
+strings.unshift('e');
+
+
+
+console.log(strings);
